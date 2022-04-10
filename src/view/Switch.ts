b02532a34b388a3be7baeefd8a79d6ad8e7b0de3
Mark-André Hopf <mhopf@mark13.org>
2022-04-10 09:38:57 +0200

    tabs: show and switch panels
    need to redesign this as the <tx-tab> elements and their content must not be
    moved inside the shadow dom so that they have access to the document's resources
    like CSS.
    
    doing it like this will also result in more sane behaviour when looking at the
    page's source code in the browser.

--- src/view/Switch.ts
+++ src/view/Switch.ts
@@ -16,7 +16,6 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
 import { input, span } from "../util/lsx"
 import { BooleanView } from "./BooleanView"
 
--- src/view/Tab.ts
+++ src/view/Tab.ts
@@ -1,9 +1,30 @@
+/*
+ *  The TOAD JavaScript/TypeScript GUI Library
+ *  Copyright (C) 2018-2021 Mark-André Hopf <mhopf@mark13.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { View } from "../view/View"
 import { ul, li, span, text, div } from "../util/lsx"
 
 export class Tabs extends View {
-    markerLine: HTMLDivElement
-    content: HTMLDivElement
+    markerLine: HTMLElement
+    content: HTMLElement
+
+    activeTab?: HTMLElement
+    activePanel?: HTMLElement
 
     constructor() {
         super()
@@ -15,7 +36,8 @@ export class Tabs extends View {
             this.classList.add("tx-vertical")
         }
 
-        let firstTab: HTMLElement | undefined
+        this.content = div()
+
         const tabContainer = ul()
         for (let i = 0; i < this.children.length; ++i) {
             const child = this.children[i]
@@ -23,7 +45,9 @@ export class Tabs extends View {
                 console.log(`unexpected <${child.nodeName.toLowerCase()}> within <tabs>`)
                 continue
             }
-            let tabLabel
+            const panel = div()
+
+            let tabLabel: HTMLElement
             tabContainer.appendChild(
                 li(
                     tabLabel = span(
@@ -31,9 +55,28 @@ export class Tabs extends View {
                     )
                 )
             )
-            tabLabel.onmousedown = this.setTab
-            if (firstTab === undefined) {
-                firstTab = tabLabel
+            tabLabel.onmousedown = (ev: MouseEvent) => {
+                ev.stopPropagation()
+                ev.preventDefault()
+                ev.cancelBubble = true
+                this.setTab(tabLabel, panel)
+            }
+
+            // for (let j = 0; j < child.childNodes.length; ++j) {
+            //     panel.appendChild(child.childNodes[j])
+            // }
+            while(child.childNodes.length > 0) {
+                panel.appendChild(child.childNodes[0])
+            }
+            
+            // panel.innerHTML = child.innerHTML
+            this.content.appendChild(panel)
+
+            if (this.activeTab === undefined) {
+                this.activeTab = tabLabel
+                this.activePanel = panel
+            } else {
+                panel.style.display = "none"
             }
         }
 
@@ -41,27 +84,16 @@ export class Tabs extends View {
         this.attachStyle("tabs")
         this.shadowRoot!.appendChild(tabContainer)
         this.shadowRoot!.appendChild(this.markerLine = div())
-        this.shadowRoot!.appendChild(this.content = div())
+        this.shadowRoot!.appendChild(this.content)
         this.markerLine.classList.add("line")
         this.content.classList.add("content")
 
-        if (firstTab !== undefined) {
-            this.setTab(firstTab)
+        if (this.activeTab) {
+            this.setTab(this.activeTab, this.activePanel!)
         }
     }
 
-    setTab(eventOrTab: MouseEvent | HTMLElement) {
-        let tab: HTMLElement
-        if (eventOrTab instanceof HTMLElement) {
-            tab = eventOrTab;
-        } else {
-            let event = eventOrTab
-            tab = event.target as HTMLElement
-            event.stopPropagation()
-            event.preventDefault()
-            event.cancelBubble = true
-        }
-
+    setTab(tab: HTMLElement, panel: HTMLElement) {
         const line = this.markerLine
         if (this.hasAttribute("vertical")) {
             line.style.top = `${tab.offsetTop}px`
@@ -71,13 +103,13 @@ export class Tabs extends View {
             line.style.width = `${tab.clientWidth}px`
         }
 
-        const tabs = tab.parentElement!.parentElement!.querySelectorAll("li > span")
-        tabs.forEach((t: any) => {
-            t.classList.remove("active")
-        })
-        tab.classList.add("active")
+        this.activeTab!.classList.remove("active")
+        this.activeTab = tab
+        this.activeTab.classList.add("active")
 
-        this.content.innerHTML = "ZACK!!"
+        this.activePanel!.style.display = "none"
+        this.activePanel = panel
+        this.activePanel.style.display = ""
     }
 }
 
