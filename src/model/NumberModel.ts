7ee161877da3ecb2892b511e51202077865b5730
Mark-André Hopf <mhopf@mark13.org>
2023-09-09 18:22:32 +0200

    NumberModel: add more tests and fix them

--- src/model/NumberModel.ts
+++ src/model/NumberModel.ts
@@ -21,9 +21,22 @@ import { ModelOptions } from "./Model"
 import { BigDecimal } from "@toad/util/BigDecimal"
 
 export interface NumberModelOptions extends ModelOptions {
+    /**
+     * when set, value will not go below min
+     */
     min?: number
+    /**
+     * when set, value will not go above max
+     */
     max?: number
+    /**
+     * when set, some views can increase/decrease the value, e.g. using the scrollwheel
+     */
     step?: number
+    /**
+     * when 'true', value will always be clipped to min/max. 'undefined' per default
+     * as it makes editing the value in a text field quite annoying.
+     */
     autocorrect?: boolean
 }
 
@@ -31,7 +44,6 @@ export interface NumberModelOptions extends ModelOptions {
  * @category Application Model
  */
 export class NumberModel extends ValueModel<number, NumberModelOptions> {
-
     constructor(value: number, options?: NumberModelOptions) {
         super(value, options)
     }
@@ -39,84 +51,101 @@ export class NumberModel extends ValueModel<number, NumberModelOptions> {
         if (this.step !== undefined) {
             const value = new BigDecimal(this.value)
             const step = new BigDecimal(this.step)
-            this.value = parseFloat( value.add(step).toString())
+            super.value = this.clip(parseFloat(value.add(step).toString()))
         }
     }
     decrement() {
         if (this.step !== undefined) {
             const value = new BigDecimal(this.value)
             const step = new BigDecimal(this.step)
-            this.value = parseFloat( value.sub(step).toString())
+            super.value = this.clip(parseFloat(value.sub(step).toString()))
         }
     }
 
     override set value(value: number | string) {
         let number = typeof value === "string" ? parseFloat(value) : value
         this.modified.withLock(() => {
-            let error: string | undefined = undefined
-            if (this.min !== undefined && number < this.min) {
-                if (this.autocorrect) {
-                    value = this.min
-                } else {
-                    error = `The value must not be below ${this.min}.`
-                }
+            if (this.autocorrect) {
+                super.value = this.clip(number)
+            } else {
+                super.value = number
+                this.error = this.check(number)
             }
-            if (this.max !== undefined && number > this.max) {
-                if (this.autocorrect) {
-                    value = this.max
-                } else {
-                    error = `The value must not be above ${this.max}.`
-                }
-            }
-            super.value = number
-            this.error = error
         })
-
     }
-    override get value(): number { return super.value }
+    override get value(): number {
+        return super.value
+    }
 
     set min(min: number | undefined) {
-        if (this.options?.min === min)
-            return
+        if (this.options?.min === min) return
         if (this.options === undefined) {
             this.options = {}
         }
         this.options.min = min
         this.modified.trigger(undefined as any)
     }
-    get min(): number | undefined { return this.options?.min }
+    get min(): number | undefined {
+        return this.options?.min
+    }
 
     set max(max: number | undefined) {
-        if (this.options?.max === max)
-            return
+        if (this.options?.max === max) return
         if (this.options === undefined) {
             this.options = {}
         }
         this.options.max = max
         this.modified.trigger(undefined as any)
     }
-    get max(): number | undefined { return this.options?.max }
+    get max(): number | undefined {
+        return this.options?.max
+    }
 
     set step(step: number | undefined) {
-        if (this.options?.step === step)
-            return
+        if (this.options?.step === step) return
         if (this.options === undefined) {
             this.options = {}
         }
         this.options.step = step
         this.modified.trigger(undefined as any)
     }
-    get step(): number | undefined { return this.options?.step }
+    get step(): number | undefined {
+        return this.options?.step
+    }
 
     set autocorrect(autocorrect: boolean | undefined) {
-        if (this.options?.autocorrect === autocorrect)
-            return
+        if (this.options?.autocorrect === autocorrect) return
         if (this.options === undefined) {
             this.options = {}
         }
         this.options.autocorrect = autocorrect
         this.modified.trigger(undefined as any)
     }
-    get autocorrect(): boolean { return this.options?.autocorrect === true }
-}
+    get autocorrect(): boolean {
+        return this.options?.autocorrect === true
+    }
 
+    private clip(value: number) {
+        if (this.min !== undefined && value < this.min) {
+            value = this.min
+        }
+        if (this.max !== undefined && value > this.max) {
+            value = this.max
+        }
+        return value
+    }
+
+    private check(value: number) {
+        if (this.min !== undefined && value < this.min) {
+            return `The value must not be below ${this.min}.`
+        }
+        if (this.max !== undefined && value > this.max) {
+            if (this.autocorrect) {
+                value = this.max
+            } else {
+                return `The value must not be above ${this.max}.`
+            }
+        }
+        return undefined
+    }
+}
--- test/model/NumberModel.spec.ts
+++ test/model/NumberModel.spec.ts
@@ -2,22 +2,96 @@ import { expect } from "@esm-bundle/chai"
 import { NumberModel } from "@toad/model/NumberModel"
 
 describe("NumberModel", function () {
-    it("increment()", function () {
-        const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
-        model.increment()
-        expect(model.value).to.equal(0.6)
-        model.increment()
-        expect(model.value).to.equal(0.7)
-        model.increment()
-        expect(model.value).to.equal(0.8)
+    describe("value", function() {
+        it("set and read", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
+            expect(model.value).to.equal(0.5)
+            model.value = 0.25
+            expect(model.value).to.equal(0.25)
+            expect(model.error).to.be.undefined
+        })
+        it("set and read below min", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
+            expect(model.value).to.equal(0.5)
+            model.value = -1
+            expect(model.value).to.equal(-1)
+            expect(model.error).to.not.be.undefined
+        })
+        it("set and read below min when autocorrect = true", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1, autocorrect: true })
+            expect(model.value).to.equal(0.5)
+            model.value = -1
+            expect(model.value).to.equal(0)
+            expect(model.error).to.be.undefined
+        })
+        it("set and read when autocorrect = true", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1, autocorrect: true })
+            expect(model.value).to.equal(0.5)
+            model.value = 0.25
+            expect(model.value).to.equal(0.25)
+            expect(model.error).to.be.undefined
+        })
+        it("set and read above max", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
+            expect(model.value).to.equal(0.5)
+            model.value = 2
+            expect(model.value).to.equal(2)
+            expect(model.error).to.not.be.undefined
+        })
+        it("set and read above max when autocorrect = true", function() {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1, autocorrect: true })
+            expect(model.value).to.equal(0.5)
+            model.value = 2
+            expect(model.value).to.equal(1)
+            expect(model.error).to.be.undefined
+        })
     })
-    it("decrement", function () {
-        const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
-        model.decrement()
-        expect(model.value).to.equal(0.4)
-        model.decrement()
-        expect(model.value).to.equal(0.3)
-        model.decrement()
-        expect(model.value).to.equal(0.2)
+    describe("increment()", function () {
+        it("increments by 'step' (without floating point errors)", function () {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
+            model.increment()
+            expect(model.value).to.equal(0.6)
+            model.increment()
+            expect(model.value).to.equal(0.7)
+            model.increment()
+            expect(model.value).to.equal(0.8)
+            expect(model.error).to.be.undefined
+        })
+        it("does not increment beyond max when autocorrect = true", function() {
+            const model = new NumberModel(0.95, { min: 0.0, max: 1.0, step: 0.1, autocorrect: true })
+            model.increment()
+            expect(model.value).to.equal(1.0)
+            expect(model.error).to.be.undefined
+        })
+        it("does not increment beyond max", function() {
+            const model = new NumberModel(0.95, { min: 0.0, max: 1.0, step: 0.1 })
+            model.increment()
+            expect(model.value).to.equal(1.0)
+            expect(model.error).to.be.undefined
+        })
+    })
+    describe("decrement()", function () {
+        it("decrements by 'step' (without floating point errors)", function () {
+            const model = new NumberModel(0.5, { min: 0.0, max: 1.0, step: 0.1 })
+            model.decrement()
+            expect(model.value).to.equal(0.4)
+            model.decrement()
+            expect(model.value).to.equal(0.3)
+            model.decrement()
+            expect(model.value).to.equal(0.2)
+            expect(model.error).to.be.undefined
+        })
+        it("does not decrement below min", function() {
+            const model = new NumberModel(0.05, { min: 0.0, max: 1.0, step: 0.1 })
+            model.decrement()
+            expect(model.value).to.equal(0.0)
+            expect(model.error).to.be.undefined
+        })
+        it("does not decrement below min when autocorrect = true", function() {
+            const model = new NumberModel(0.05, { min: 0.0, max: 1.0, step: 0.1, autocorrect: true })
+            model.decrement()
+            expect(model.value).to.equal(0.0)
+            expect(model.error).to.be.undefined
+        })
     })
 })
