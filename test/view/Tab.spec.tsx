5b09b7a3149c5b4e05e6f5967fa5e46eb46ae9e6
Mark-André Hopf <mhopf@mark13.org>
2023-12-17 10:41:25 +0100

    tab: update tests

--- test/view/Tab.spec.tsx
+++ test/view/Tab.spec.tsx
@@ -2,14 +2,12 @@ import { expect } from '@esm-bundle/chai'
 import { unbind } from "@toad/controller/globalController"
 import { Tabs, Tab } from "@toad/view/Tab"
 import { EnumModel } from '@toad/model/EnumModel'
-import { html } from '@toad/util/lsx'
 
 import { style as txBase } from "@toad/style/tx"
 import { style as txStatic } from "@toad/style/tx-static"
 import { style as txDark } from "@toad/style/tx-dark"
 
 import { getByText, click } from "../testlib"
-import { HTMLElementProps, View } from '@toad'
 
 describe("view", function () {
 
@@ -20,86 +18,6 @@ describe("view", function () {
     })
 
     describe("Tab", function () {
-        describe("partial render", function () {
-            it.only("foo", function () {
-
-                // const a = "ToolButtonPanel".replace(/[A-Z]/g, "-$&").toLowerCase()
-                // console.log(a)
-
-                class A extends View {
-                    constructor(props?: HTMLElementProps) {
-                        console.log("construct A")
-                        super(props)
-                    }
-                }
-
-                interface BProps extends HTMLElementProps {
-                    content?: () => any
-                }
-
-                class B extends View {
-                    content?: () => any
-
-                    constructor(props?: BProps) {
-                        console.log("construct B")
-                        super(props)
-                        this.content = props?.content
-                        // this.open()
-                        // console.log(props?.content)
-                        // throw Error("not now")
-                    }
-                    // we provide these as methods in case someone want's to override
-                    // the behaviour, e.g. to create/delete the content
-                    open() {
-                        if (this.childNodes.length === 0 && this.content) {
-                            this.replaceChildren(this.content())
-                        }
-                    }
-                    close() {}
-                }
-
-                class C extends View {
-                    constructor(props?: HTMLElementProps) {
-                        console.log("construct C")
-                        super(props)
-                    }
-                }
-                View.define("tx-a", A)
-                View.define("tx-b", B)
-                View.define("tx-c", C)
-
-                const out =
-                    <A>
-                        <B content={() => <C>Tab1</C>}/>
-                        <B content={() => <C>Tab2</C>}/>
-                    </A>
-                console.log(out)
-            })
-        })
-        describe("HTML", () => {
-            it("no model, click changes tab", function () {
-                document.body.innerHTML = html`
-                    <tx-tabs>
-                        <tx-tab label="Uno">T1</tx-tab>
-                        <tx-tab label="Duo">T2</tx-tab>
-                        <tx-tab label="Tres">T3</tx-tab>
-                    </tx-tabs>
-                `
-                const t1 = getByText("T1") as HTMLElement
-                const t2 = getByText("T2") as HTMLElement
-                const t3 = getByText("T3") as HTMLElement
-
-                expect(t1.style.display).to.equal("")
-                expect(t2.style.display).to.equal("none")
-                expect(t3.style.display).to.equal("none")
-
-                click(getByText("Duo")!)
-
-                expect(t1.style.display).to.equal("none")
-                expect(t2.style.display).to.equal("")
-                expect(t3.style.display).to.equal("none")
-            })
-        })
         describe("JSX", function () {
             it("no model, click changes tab", function () {
                 document.body.replaceChildren(
@@ -145,7 +63,7 @@ describe("view", function () {
                 expect(t3.style.display).to.equal("none")
             })
 
-            it("model changes with active tab", function () {
+            it("model changes when active tab changes", function () {
 
                 enum TAB { UNO, DUO, TRES }
                 const model = new EnumModel(TAB.UNO, TAB)
